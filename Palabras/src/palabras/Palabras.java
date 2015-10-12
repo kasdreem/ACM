9add24df1cdb30d24a6bb1d6374fdc159b68e128
Usuario <Usuario@192.168.1.34>
2015-10-12 19:22:32 -0300

    ACM/domino

--- Palabras/src/palabras/Palabras.java
+++ Palabras/src/palabras/Palabras.java
@@ -19,42 +19,28 @@ public class Palabras {
      */
      
     public static void main(String[] args) {
+        int n=0;
+        
         Scanner in = new Scanner(System.in);
-		int a = in.nextInt();		
-		
-		for(int i=0; i<a; i++){
-			String palabra = in.next();
-			if(palabra.length()>=10){
-				int largo = palabra.length()-2;
-				System.out.print((palabra.charAt(0))+""+largo+""+(palabra.charAt(palabra.length()-1)));
-			}else{
-				System.out.println(palabra);
-			}
-				}
-       // System.out.println(a);
+        int a = in.nextInt();		
+        String palabra[]=new String[a];
+        for(int i=0; i<a; i++){
+            
+        palabra[i] = in.next();
         
-       // for(int i=0;i<n;i++){
-         //   System.out.println(palabra.charAt(i));
-        //}
+	
         
-      /**  
-        Scanner sc = new Scanner( System.in); 
-
-     System.out.print( "Ingrese una frase: "); 
-     String frase = sc.nextLine(); 
-     int cont[] = new int[26]; 
-
-     for(int i = 0; i < frase.length(); i++){ 
-          char c = Character.toLowerCase( frase.charAt(i)); 
-               if(c >= 'a' && c <= 'z' ) 
-                    cont[c - 'a']++; 
-     } 
-
-     for(int i = 0; i < 26; i++) {
-          if(cont[i] > 0) 
-               System.out.println( (char)(i + 'a') ); 
-}
-    }*/
+	}
+        for(int i=0;i<a;i++){
+        n=palabra[i].length();
+        if(palabra[i].length()>10){
+        int largo = palabra[i].length()-2;
+	System.out.println((palabra[i].charAt(0))+""+largo+""+(palabra[i].charAt(n-1)));
+	}else{
+	System.out.println(palabra[i]);
+        }
+        }
+      
     
 }
 }
